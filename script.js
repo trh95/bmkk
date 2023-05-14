d98dddb39ba0b923e9b668fa7d0a535f9da84dd9
Tamás Horváth <tamasrhorvath@gmail.com>
2023-05-14 21:27:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 //---MAIN MOVING---
 
-let text = document.getElementById('text');
-let vulture = document.getElementById('vulture');
-let back1 = document.getElementById('back1');
-let front1 = document.getElementById('front1');
-let nav = document.getElementById('nav');
+
+let cloud1 = document.getElementById('cloud1');
+let cloud2 = document.getElementById('cloud2');
+let back = document.getElementById('back');
+let front = document.getElementById('front');
+
 
 
 window.addEventListener('scroll', function () {
     let value = window.scrollY;
 
-    text.style.top = 70 + value * -0.22 + '%';
+    
     back.style.top = value * 0.3 + 'px';
     cloud1.style.top = value * -0.5 + 'px';
     cloud1.style.left = value * -0.5 + 'px';
